69b3ca4de75560a726f514ac3b9890d655bcebaa
Valentin Venzhega <valek917@yandex.ru>
2021-03-05 22:32:33 +0300

    добавил комментарии

--- js/script.js
+++ js/script.js
@@ -1,49 +1,50 @@
-const filterByType = (type, ...values) => values.filter(value => typeof value === type),
+const filterByType = (type, ...values) => values.filter(value => typeof value === type), // проверяем на соответствие типу введенных данных 
 
 	hideAllResponseBlocks = () => {
-		const responseBlocksArray = Array.from(document.querySelectorAll('div.dialog__response-block'));
-		responseBlocksArray.forEach(block => block.style.display = 'none');
+		const responseBlocksArray = Array.from(document.querySelectorAll('div.dialog__response-block')); // получаем коллекцию со страницы
+		responseBlocksArray.forEach(block => block.style.display = 'none'); // перебираем и скрываем блок
+
 	},
 
-	showResponseBlock = (blockSelector, msgText, spanSelector) => {
+	showResponseBlock = (blockSelector, msgText, spanSelector) => { //функция показывает блок в зависимости от резуьтата
 		hideAllResponseBlocks();
-		document.querySelector(blockSelector).style.display = 'block';
+		document.querySelector(blockSelector).style.display = 'block'; // добавляет блок на страницу
 		if (spanSelector) {
-			document.querySelector(spanSelector).textContent = msgText;
+			document.querySelector(spanSelector).textContent = msgText; // записываем в span
 		}
 	},
 
-	showError = msgText => showResponseBlock('.dialog__response-block_error', msgText, '#error'),
+	showError = msgText => showResponseBlock('.dialog__response-block_error', msgText, '#error'), // функция для обработки ошибки
 
-	showResults = msgText => showResponseBlock('.dialog__response-block_ok', msgText, '#ok'),
+	showResults = msgText => showResponseBlock('.dialog__response-block_ok', msgText, '#ok'), // функция для обработки положительного результата
 
-	showNoResults = () => showResponseBlock('.dialog__response-block_no-results'),
+	showNoResults = () => showResponseBlock('.dialog__response-block_no-results'), // функция для обработки если данные не были введены
 
-	tryFilterByType = (type, values) => {
+	tryFilterByType = (type, values) => { // функция обработки ошибок
 		try {
-			const valuesArray = eval(`filterByType('${type}', ${values})`).join(", ");
-			const alertMsg = (valuesArray.length) ?
+			const valuesArray = eval(`filterByType('${type}', ${values})`).join(", "); // получае данные из инпута в зависимости от выбранного селектора
+			console.log(valuesArray);
+			const alertMsg = (valuesArray.length) ? //делаем проверку на соответствие выбранным типам
 				`Данные с типом ${type}: ${valuesArray}` :
 				`Отсутствуют данные типа ${type}`;
 			showResults(alertMsg);
-		} catch (e) {
+		} catch (e) { // если ошибка выводим сообщение
 			showError(`Ошибка: ${e}`);
 		}
 	};
 
-const filterButton = document.querySelector('#filter-btn');
+const filterButton = document.querySelector('#filter-btn'); // получили кнопку 
 
-filterButton.addEventListener('click', e => {
-	const typeInput = document.querySelector('#type');
-	const dataInput = document.querySelector('#data');
+filterButton.addEventListener('click', e => { // навешиваем событие click
+	const typeInput = document.querySelector('#type'); //получаем selcetor
+	const dataInput = document.querySelector('#data'); // получаем инпут ввода данных
 
-	if (dataInput.value === '') {
-		dataInput.setCustomValidity('Поле не должно быть пустым!');
+	if (dataInput.value === '') { // проверка на пустоту
+		dataInput.setCustomValidity('Поле не должно быть пустым!'); //Метод устанавливает специальное сообщение для выбранного элемента
 		showNoResults();
 	} else {
-		dataInput.setCustomValidity('');
-		e.preventDefault();
-		tryFilterByType(typeInput.value.trim(), dataInput.value.trim());
+		dataInput.setCustomValidity(''); //элемент не имеет пользовательской ошибки в параметре укажите пустую строку 
+		e.preventDefault(); // отменяем стандартное поведение браузера
+		tryFilterByType(typeInput.value.trim(), dataInput.value.trim()); // вызываем функцию обработки ошибки
 	}
 });
-
